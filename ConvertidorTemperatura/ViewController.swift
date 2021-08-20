c6e650ebf622eb59eb0a8017660dd0300a4b6381
thenutscout <jesus-e98@hotmail.com>
2021-08-19 19:28:14 -0500

    first commit

---
+++ ConvertidorTemperatura/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ConvertidorTemperatura
+//
+//  Created by user194082 on 8/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var fahrenheitTextField: UITextField!
+    @IBOutlet weak var celsiusTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func convert(_ sender: UIButton) {
+    }
+}
+
